fe007440a9bbe4986ff97e34f8ad4b0c13cc5ce8
mihai-burduselu <michelcatalin@gmail.com>
2016-11-23 01:16:11 +0200

    Add the project

---
+++ HelloWorldApi/Controllers/GreetingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace HelloWorldApi.Controllers
+{
+    public class GreetingController : ApiController
+    {
+        [HttpGet]
+        public string getGreeting()
+        {
+            string greeting = "Hello, World!";
+            return greeting;
+        }
+    }
+}
